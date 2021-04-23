52a2e849795847ea125653ec615922af13f80e6f
yhira <info@nelog.jp>
2021-04-23 18:18:48 +0900

    Revert "Revert "アイコンリストテスト""
    This reverts commit bb0577bbbd84859993818f107296d955996f0fd9.

--- blocks/src/block/icon-list/deprecated.js
+++ blocks/src/block/icon-list/deprecated.js
@@ -4,10 +4,9 @@
  * @link: https://wp-cocoon.com/
  * @license: http://www.gnu.org/licenses/gpl-2.0.html GPL v2 or later
  */
-import { THEME_NAME, BUTTON_BLOCK, CLICK_POINT_MSG, colorValueToSlug } from '../../helpers';
+import { colorValueToSlug } from '../../helpers';
 import classnames from 'classnames';
 
-const { __ } = wp.i18n;
 const { InnerBlocks, RichText } = wp.editor;
 
 export const deprecated = [
